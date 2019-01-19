4163374e928511ec57f43b8acc3e72db3e75f394
jcrossley6 <johncrossley2014@virginmedia.com>
2019-01-19 18:58:18 +0000

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,34 @@
+﻿
+(function () {
+    "use strict";
+
+    var messageBanner;
+
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            // Initialize the FabricUI notification mechanism and hide it
+            var element = document.querySelector('.ms-MessageBanner');
+            messageBanner = new fabric.MessageBanner(element);
+            messageBanner.hideBanner();
+
+
+
+            // TODO1: Assign event handler for insert-image button.
+            // TODO4: Assign event handler for insert-text button.
+            // TODO6: Assign event handler for get-slide-metadata button.
+            // TODO8: Assign event handlers for the four navigation buttons.
+        });
+    };
+
+   
+    
+   
+
+    // Helper function for displaying notifications
+    function showNotification(header, content) {
+        $("#notification-header").text(header);
+        $("#notification-body").text(content);
+        messageBanner.showBanner();
+        messageBanner.toggleExpansion();
+    }
+})();
